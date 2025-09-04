f040f1fd366653f278f47b2c9a05beaa3195eb9a
Ericlaucian <ericlaucian@gmail.com>
2025-09-04 16:18:56 -0300

    sorted feito

--- alg/merge/sorted_merge_3way.cpp
+++ alg/merge/sorted_merge_3way.cpp
@@ -1,4 +1,4 @@
-/* R. Fabbri, 2024 */
+/*R. Fabbri, 2024 */
 #include "sorted_merge_3way.h"
 #include <climits>
 
@@ -10,12 +10,33 @@ sorted_merge_3way(
     int *list_abc)
 {
 
-  /* 
+	int j{0}, k{0}, l{0}, last{INT_MIN};
+    	for (int i = 0; i < na + nb + nc; ++i) {
+        	int a = list_a[j], b = list_b[k], c = list_c[l];
 
-   Entrar coom aqui 
-   <++>
-   
-  */
+        	if (j >= na) {a = INT_MAX;}
+        	if (k >= nb) {b = INT_MAX;}
+        	if (l >= nc) {c = INT_MAX;}
 
-  return false; // stub
+        	if (a <= b && a <= c) {
+        		list_abc[i] = a;
+			if (a < last) return false;
+        	    	++j;
+        	}
+        	if (b <= a && b <= c) {
+			if (b < last) return false;
+        		list_abc[i] = b;
+        	    	++k;
+        	}
+		if(c <= a && c <= b){
+			if (c < last) return false;
+	       		list_abc[i] = c;
+        		++l;
+		}
+
+		last = list_abc[i];
+    	}
+
+ 	 return true; // stub
+
 }
